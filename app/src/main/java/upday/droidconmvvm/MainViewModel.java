3b2a42afb06573cbc96d9e1730f306f99d7328bb
Amirhossein Hosseini <Blackworld.2035@gmail.com>
2018-02-16 15:46:58 +0400

    MainViewModel Observables edited to package local

--- app/src/main/java/upday/droidconmvvm/MainViewModel.java
+++ app/src/main/java/upday/droidconmvvm/MainViewModel.java
@@ -31,7 +31,7 @@ public class MainViewModel {
     }
 
     @NonNull
-    public Observable<String> getGreeting() {
+    Observable<String> getGreeting() {
         return mSelectedLanguage
                 .observeOn(mSchedulerProvider.computation())
                 .map(Language::getCode)
@@ -39,11 +39,11 @@ public class MainViewModel {
     }
 
     @NonNull
-    public Observable<List<Language>> getSupportedLanguages() {
+    Observable<List<Language>> getSupportedLanguages() {
         return mDataModel.getSupportedLanguages();
     }
 
-    public void languageSelected(@NonNull final Language language) {
+    void languageSelected(@NonNull final Language language) {
         mSelectedLanguage.onNext(language);
     }
 
